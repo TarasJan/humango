56b3b11e2509baa106e901645e8336d87d445028
Jan Taras <jan.taras29@gmail.com>
2020-11-11 23:25:53 +0100

    [11.11.2020] Wordify first release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# wordify
-Go library for converting numeric input to text
+# Wordify <img alt="Travis (.org)" src="https://img.shields.io/travis/TarasJan/wordify">
+Wordify is a simple Go library for converting numeric input into human readable text. 
+
+It can be used for humanizing currencies, ledger items, stock or other items that require numbers written in plaintext.
+
+# Usage
+
+The library exposes the single method `Wordify` that handles whole numbers and floats up to the second decimal point.
+
+```go
+import github.com/TarasJan/wordify
+
+wordify.Wordify(2020)
+// Returns "two thousand twenty"
+
+wordify.Wordify(-771)
+// Returns "minus seven hundred seventy one"
+
+wordify.Wordify(42.94)
+// Returns "forty two point ninety four"
+
+```
+
+# Upcoming Features
+
+Currently only English numerals are supported though other languages are in the works. Future support for formatting currencies and common units akin to Rails' [number_to_human](https://apidock.com/rails/v5.2.3/ActionView/Helpers/NumberHelper/number_to_human) is planned.
+
+The software is licensed under MIT License.
---
+++ wordify_test.go
@@ -0,0 +1,192 @@
+package wordify
+
+import "testing"
+
+func TestIntZero(t *testing.T) {
+	result, _ := Wordify(0)
+	if result != "zero" {
+		t.Errorf("Wordify(0) did not return \"zero\"")
+	}
+}
+
+func TestIntFive(t *testing.T) {
+	result, _ := Wordify(5)
+	if result != "five" {
+		t.Errorf("Wordify(5) did not return \"five\"")
+	}
+}
+
+func TestIntMinusSeven(t *testing.T) {
+	result, _ := Wordify(-7)
+	if result != "minus seven" {
+		t.Errorf("Wordify(-7) did not return \"minus seven\"")
+	}
+}
+
+func TestIntEleven(t *testing.T) {
+	result, _ := Wordify(11)
+	if result != "eleven" {
+		t.Errorf("Wordify(11) did not return \"eleven\"")
+	}
+}
+
+func TestIntNineteen(t *testing.T) {
+	result, _ := Wordify(19)
+	if result != "nineteen" {
+		t.Errorf("Wordify(19) did not return \"nineteen\"")
+	}
+}
+
+func TestIntMinusTwelve(t *testing.T) {
+	result, _ := Wordify(-12)
+	if result != "minus twelve" {
+		t.Errorf("Wordify(-12) did not return \"minus twelve\"")
+	}
+}
+
+func TestIntTwentyThree(t *testing.T) {
+	result, _ := Wordify(23)
+	if result != "twenty three" {
+		t.Errorf("Wordify(23) did not return \"twenty three\"")
+	}
+}
+
+func TestIntEightySeven(t *testing.T) {
+	result, _ := Wordify(87)
+	if result != "eighty seven" {
+		t.Errorf("Wordify(87) did not return \"eighty seven\"")
+	}
+}
+
+func TestIntMinusThirty(t *testing.T) {
+	result, _ := Wordify(-30)
+	if result != "minus thirty" {
+		t.Errorf("Wordify(-30) did not return \"minus thirty\"" + " \\ " + result)
+	}
+}
+
+func TestIntNinetyNine(t *testing.T) {
+	result, _ := Wordify(99)
+	if result != "ninety nine" {
+		t.Errorf("Wordify(99) did not return \"ninety nine\"" + " \\ " + result)
+	}
+}
+
+func TestIntOneHundredOne(t *testing.T) {
+	result, _ := Wordify(101)
+	if result != "one hundred one" {
+		t.Errorf("Wordify(101) did not return \"one hundred one\"" + " \\ " + result)
+	}
+}
+
+func TestIntMinusNineHundredTwentyThree(t *testing.T) {
+	result, _ := Wordify(-923)
+	if result != "minus nine hundred twenty three" {
+		t.Errorf("Wordify(-923) did not return \"minus nine hundred twenty three\"" + " \\ " + result)
+	}
+}
+
+func TestIntOneThousand(t *testing.T) {
+	result, _ := Wordify(1000)
+	if result != "one thousand" {
+		t.Errorf("Wordify(2000) did not return \"one thousand\"" + " \\ " + result)
+	}
+}
+
+func TestIntTwoThousand(t *testing.T) {
+	result, _ := Wordify(2000)
+	if result != "two thousand" {
+		t.Errorf("Wordify(2000) did not return \"two thousand\"" + " \\ " + result)
+	}
+}
+
+func TestIntThousandNineteen(t *testing.T) {
+	result, _ := Wordify(1019)
+	if result != "one thousand nineteen" {
+		t.Errorf("Wordify(1019) did not return \"one thousand nineteen\"" + " \\ " + result)
+	}
+}
+
+func TestIntFourHundredSeventeenThousand(t *testing.T) {
+	result, _ := Wordify(417000)
+	if result != "four hundred seventeen thousand" {
+		t.Errorf("Wordify(417000) did not return \"four hundred seventeen thousand\"" + " \\ " + result)
+	}
+}
+
+func TestIntFortyThreeThousandFourHundredTwelve(t *testing.T) {
+	result, _ := Wordify(43412)
+	if result != "forty three thousand four hundred twelve" {
+		t.Errorf("Wordify(43412) did not return \"forty three thousand four hundred twelve\"" + " \\ " + result)
+	}
+}
+
+func TestIntMillion(t *testing.T) {
+	result, _ := Wordify(1000000)
+	if result != "one million" {
+		t.Errorf("Wordify(1000000) did not return \"one million\"" + " \\ " + result)
+	}
+}
+
+func TestIntMillionTwoHundredFiftyThousand(t *testing.T) {
+	result, _ := Wordify(1250000)
+	if result != "one million two hundred fifty thousand" {
+		t.Errorf("Wordify(1250000) did not return \"one million two hundred fifty thousand\"" + " \\ " + result)
+	}
+}
+
+func TestIntTwentyMillionOne(t *testing.T) {
+	result, _ := Wordify(20000001)
+	if result != "twenty million one" {
+		t.Errorf("Wordify(20000001) did not return \"twenty million one\"" + " \\ " + result)
+	}
+}
+
+func TestIntThreeHundredSeventySevenMillionFiveHundredTwentyEightThousandSevenHundredSixtyNine(t *testing.T) {
+	result, _ := Wordify(377528769)
+	if result != "three hundred seventy seven million five hundred twenty eight thousand seven hundred sixty nine" {
+		t.Errorf("Wordify(377528769) did not return \"three hundred seventy seven million five hundred twenty eight thousand seven hundred sixty nine\"" + " \\ " + result)
+	}
+}
+
+func TestFloatZero(t *testing.T) {
+	result, _ := Wordify(0.0)
+	if result != "zero" {
+		t.Errorf("Wordify(0) did not return \"zero\"")
+	}
+}
+
+func TestFloatSevenPointTwentyFour(t *testing.T) {
+	result, _ := Wordify(7.24)
+	if result != "seven point twenty four" {
+		t.Errorf("Wordify(7.24) did not return \"seven point twenty four\"")
+	}
+}
+
+func TestFloatThirtySixZeroZero(t *testing.T) {
+	result, _ := Wordify(36.00)
+	if result != "thirty six" {
+		t.Errorf("Wordify(36.00) did not return \"thirty six\"")
+	}
+}
+
+func TestFloatFortyTwoPointOne(t *testing.T) {
+	result, _ := Wordify(42.01)
+	if result != "forty two point one" {
+		t.Errorf("Wordify(42.01) did not return \"forty two point one\"" + result)
+	}
+}
+
+func TestFloatMinusNintyPointTen(t *testing.T) {
+	result, _ := Wordify(-90.1)
+	if result != "minus ninety point ten" {
+		t.Errorf("Wordify(90.1) did not return \"ninety point ten\"" + result)
+	}
+}
+
+func TestOtherTypesNotSupported(t *testing.T) {
+	result, err := Wordify(true)
+	if err == nil || result != "" {
+		t.Errorf("Wordify(true), did not return an error with empty response")
+	}
+}
